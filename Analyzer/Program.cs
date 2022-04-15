42956d4fed1199a98f4565d11372f53dad6bbd26
Bolanle Ayodeji <abideklove@gmail.com>
2022-04-15 11:04:09 +0100

    initial commit

---
+++ Analyzer/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("19.10.2019 21:16:50:3694 ".Length);
---
+++ LogAnalyzer.Lib/Analyze.cs
@@ -0,0 +1,21 @@
+﻿namespace LogAnalyzer.Lib
+{
+    public class Analyze
+    {
+        public HashSet<string> UniqueRecords { get; set; } = new HashSet<string>();
+        public int Count { get; set; }
+        public void Search(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            var line = sr.ReadLine();
+
+            while (line != null)
+            {
+                line = line.Substring(25);
+                UniqueRecords.Add(line);
+                Count++;
+            }
+        }
+    }
+}
